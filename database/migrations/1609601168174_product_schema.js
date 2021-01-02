8202245352a66342bcab50f5f28ef7298e57fe11
helionporto <helionporto@hotmail.com>
2021-01-02 12:43:54 -0300

    migration Category e Product e relacionamento Image/Product

---
+++ database/migrations/1609601168174_product_schema.js
@@ -0,0 +1,48 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProductSchema extends Schema {
+  up () {
+    this.create('products', (table) => {
+      table.increments()
+      table.string('name', 100)
+      table.text('description')
+      table.decimal('price', 12, 2)
+      table.integer('image-id').unsigned()
+      table.timestamps()
+
+      table
+        .foreign('image_id')
+        .references('id')
+        .inTable('images')
+        .onDelete('cascade')
+    })
+
+    this.create('image_product', (table) => {
+      table.increments()
+      table.integer('image_id').unsigned()
+      table.integer('product_id').unsigned()
+
+      table
+        .foreign('image_id')
+        .references('id')
+        .inTable('images')
+        .onDelete('cascade')
+
+      table
+        .foreign('product_id')
+        .references('id')
+        .inTable('products')
+        .onDelete('cascade')
+    })
+  }
+
+  down () {
+    this.drop('products'),
+    this.drop('image_product')
+  }
+}
+
+module.exports = ProductSchema
